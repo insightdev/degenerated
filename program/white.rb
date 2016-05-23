7bd86aa1c44cd26fccd5a266f215fa0f1c9740ad
Alex Balgavy <a.balgavy@gmail.com>
2016-05-23 20:57:33 +0200

    Now only changes gray things in main method
    It originally threw errors in netbeans because it removed generated
    code around the initialization method too. That’s fixed though.

--- program/white.rb
+++ program/white.rb
@@ -1,8 +1,15 @@
 #!/bin/ruby
-infile=gets.chomp
-infileForOut = infile.split(".")
-outfile=infileForOut.first+"_edit."+infileForOut.last
 
-File.open(outfile,"w") do |out|
-	out << File.open(infile).read.gsub(/\/\/GEN-.*$/,'')
+puts "Enter your file:"
+infile = gets.chomp
+outfile=(infile.split(".")).first+"_edit."+(infile.split(".")).last
+
+File.open(outfile,'w') do |outfile|
+	File.open(infile,'r').each do |line|
+		if (line.include? "GEN-FIRST")
+			outfile.print line.sub(/\/\/GEN-(FIRST|LAST).*$/,'')
+		else
+			outfile.print line
+		end
+	end
 end
